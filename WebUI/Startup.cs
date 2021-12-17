5ecbb7cf79d4cbbe55c4aa66821aaf41174e599f
hafeezmunawar <47921756+hafeezmunawar@users.noreply.github.com>
2021-12-17 16:07:56 +0300

    RRW-1 fix: folder structure cleanup, take 2

--- WebUI/Startup.cs
+++ WebUI/Startup.cs
@@ -1,11 +1,18 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-namespace RallyRaidWebApi
+namespace WebUI
 {
     public class Startup
     {
@@ -23,7 +30,7 @@ namespace RallyRaidWebApi
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "RallyRaidWebApi", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebUI", Version = "v1" });
             });
         }
 
@@ -34,7 +41,7 @@ namespace RallyRaidWebApi
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "RallyRaidWebApi v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebUI v1"));
             }
 
             app.UseHttpsRedirection();
