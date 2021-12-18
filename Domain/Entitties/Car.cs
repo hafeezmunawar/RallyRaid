07585d4583e78c45ff78ca611945baa83e0a811f
hafeezmunawar <47921756+hafeezmunawar@users.noreply.github.com>
2021-12-18 22:37:51 +0300

    RRW-1 feature: Added Car Model and Cars Controller
    Added Car Model to Domain Project and Car Controller to WebUI Project

---
+++ Domain/Entitties/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Domain.Entitties
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public String TeamName { get; set; }
+        public int Speed { get; set; }
+        public double MelfunctionChance { get; set; }
+        public int MelfunctionsOccured { get; set; }
+        public int DistanceCoveredInMiles { get; set; }
+        public int FinishedRace { get; set; }
+        public int RacedHours { get; set; }
+    }
+}
---
+++ WebUI/Features/Cars/CarsController.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Entitties;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace WebUI.Features.Cars
+{
+    [Route("api/cars")]
+    [ApiController]
+    public class CarsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Car>> GetCars()
+        {
+            var carList = new List<Car>
+            {
+
+                new Car { TeamName = "Team A", Speed = 100, MelfunctionChance = 0.2},
+                new Car { TeamName = "Team B", Speed = 120, MelfunctionChance = 0.3},
+                new Car { TeamName = "Team C", Speed = 98, MelfunctionChance = 0.1},
+                new Car { TeamName = "Team D", Speed = 101, MelfunctionChance = 0.5}
+
+            };
+
+            return Ok(carList);
+        }
+    }
+}
